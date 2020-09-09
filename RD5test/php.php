83753009866ab267ed78f2c19d3ae90377c93937
ryu <ryu64j@gmail.com>
2020-09-09 09:40:27 +0800

    RD5test fin

--- RD5test/php.php
+++ RD5test/php.php
@@ -19,20 +19,51 @@
         multi;
         $Accountresult = mysqli_query($link, $sqlAccount);
         $Accountrow = mysqli_fetch_assoc($Accountresult);
+        $_SESSION['name'] = $Accountrow['userName'];
         echo json_encode($Accountrow);
     }
     if(isset($_POST['id'])){
+        $array2 = [];
+        $array3 = [];
         $array = [];
+        $b = [];
         $_SESSION["id"] = $_POST["id"];
         $userID = $_SESSION["id"] ;
-        $timerecordsql = "select recordID,userID, money, time, total from record where userID = '$userID' order by time desc";
+        $timerecordsql = "select u.userName as name ,recordID,r.userID, money, time, total from record as r 
+        JOIN users as u 
+        on u.userID = r.userID where r.userID = '$userID' order by time desc";
         $timeresult = mysqli_query($link,$timerecordsql);
         while($timerecord = mysqli_fetch_assoc($timeresult )){
             $array[] = $timerecord;
         }
+        $monthrecordsql = "select * from record where date(`time`) BETWEEN date_sub(curdate(),interval 60 day) and date_sub(curdate(),interval 30 day) and userID =  $userID order by time desc";
+        $monthresult = mysqli_query($link,$monthrecordsql);
+        while($monthrecord = mysqli_fetch_assoc($monthresult )){
+            $array2[] = $monthrecord;
+        }
+        $weekrecordsql = "select * from record where date(`time`) BETWEEN date_sub(curdate(),interval 7 day) and date_sub(curdate(),interval 0 day) and userID =  $userID order by time desc";
+        $weekresult = mysqli_query($link,$weekrecordsql);
+        while($weekrecord = mysqli_fetch_assoc($weekresult )){
+            $array3[] = $weekrecord;
+        }
         $_SESSION['total'] = $array[0]["total"];
-        echo json_encode($array);
+        $name = $_SESSION['name'];
+        $b[] = $array ;
+        $b[] = $name;
+        $b[] = $array2;
+        $b[] = $array3;
+        echo json_encode($b);
     }
+    // if(isset($_POST['month'])){
+    //     $array2 = [];
+    //     $userID = $_POST['month'] ;
+    //     $monthrecordsql = "select * from record where date(`time`) BETWEEN date_sub(curdate(),interval 2 day) and date_sub(curdate(),interval 1 day) and userID =  $userID order by time desc";
+    //     $monthresult = mysqli_query($link,$monthrecordsql);
+    //     while($monthrecord = mysqli_fetch_assoc($monthresult )){
+    //         $array2[] = $monthrecord;
+    //     }
+    //     echo json_encode($array);
+    // }
     if(isset($_POST['saveMoney'])){
         $savemoney = $_POST["saveMoney"];
         $userID = $_SESSION["id"];
